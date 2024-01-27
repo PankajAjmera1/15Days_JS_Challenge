f4725b63b90ac08c1317dfe6ae4e139413c16d5b
Pankaj Ajmera <coderajmera@gmail.com>
2024-01-27 19:50:37 +0530

    Day 11

---
+++ Day 11/async.js
@@ -0,0 +1,13 @@
+// Default JS --> Synchronous , Single THreaded
+
+// Execution Context -->execution one line of code at a time 
+// each execution waits for last one to complete
+
+
+// Blocking (Read file sync) v/s Non Blocking (read file async)
+
+
+// JS Engine --> MEMORY HEAP + CALL STACK
+
+
+//web Api ----->>>>Dom API , Set Timeout ,Set Interval ,fetch
