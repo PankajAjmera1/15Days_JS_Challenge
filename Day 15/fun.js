38fa1567b98a8bb9aae811d1b11bd16ae712bdbc
Pankaj Ajmera <coderajmera@gmail.com>
2024-01-31 19:10:07 +0530

    Day 15

---
+++ Day 15/fun.js
@@ -0,0 +1,30 @@
+const myArr =[]
+//  %DDebugPrint(myArr)
+
+//  continous and holey  -->> two
+
+
+// SMI  -->Integer
+// Packerd element
+
+// Double -->> float ,string,function
+
+// array ke andar elements 
+
+
+
+const arrTTwo - [1,2,3,4,5]
+// Packed_SMI_Elements
+
+arrTTwo.push(6.0)
+// packed double
+
+arrTTwo.push(7)
+
+arrTTwo[10]  =15
+//holy 
+
+
+
+
+// holey are bery expensive in js
